a477c2911407f7e7155086e0896c4fd2c4a130bb
Doudou Nan <ddnan228@gmail.com>
2020-11-23 22:58:42 -0800

    Updates

---
+++ precache-manifest.c736a168e456cb78840439370dafaa4a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "144746ba898578e3ea7e3d3bc4e67367",
+    "url": "/seainfo6150-final-project-webapp/index.html"
+  },
+  {
+    "revision": "cecc9da16e9482ddcb50",
+    "url": "/seainfo6150-final-project-webapp/static/css/main.6a1cf55d.chunk.css"
+  },
+  {
+    "revision": "73148bcabdeb9ad723b3",
+    "url": "/seainfo6150-final-project-webapp/static/js/2.88c0311f.chunk.js"
+  },
+  {
+    "revision": "cecc9da16e9482ddcb50",
+    "url": "/seainfo6150-final-project-webapp/static/js/main.10ef1c7f.chunk.js"
+  },
+  {
+    "revision": "940ec4a42937c37ae625",
+    "url": "/seainfo6150-final-project-webapp/static/js/runtime~main.c4d7f5e8.js"
+  },
+  {
+    "revision": "a4d4c62417bba87755864c61c2611e71",
+    "url": "/seainfo6150-final-project-webapp/static/media/3to5Category.a4d4c624.png"
+  },
+  {
+    "revision": "587a949d7454453440e9c3bf9659c5cd",
+    "url": "/seainfo6150-final-project-webapp/static/media/6to8Category.587a949d.png"
+  },
+  {
+    "revision": "1f37ab6ea35b28259374b3fe9114f392",
+    "url": "/seainfo6150-final-project-webapp/static/media/9to12Category.1f37ab6e.png"
+  },
+  {
+    "revision": "202afc9d1e903edfad75830ce4ce89c7",
+    "url": "/seainfo6150-final-project-webapp/static/media/Logo-large.202afc9d.png"
+  },
+  {
+    "revision": "cd32bad0eb423ef1f4a8cb90f65c02bd",
+    "url": "/seainfo6150-final-project-webapp/static/media/Logo-medium.cd32bad0.png"
+  },
+  {
+    "revision": "f52b1ccd28ea289cfcbb6dbe33eb18f9",
+    "url": "/seainfo6150-final-project-webapp/static/media/Logo-small.f52b1ccd.png"
+  },
+  {
+    "revision": "ca71897b59332c2edcf9e4e4f7d25dde",
+    "url": "/seainfo6150-final-project-webapp/static/media/babyCategory.ca71897b.png"
+  },
+  {
+    "revision": "f06ed4436aa0490dfd089796863a99e4",
+    "url": "/seainfo6150-final-project-webapp/static/media/errorImage.f06ed443.png"
+  },
+  {
+    "revision": "ba145de47c4c8039612893aacf7d3ae1",
+    "url": "/seainfo6150-final-project-webapp/static/media/logo-header.ba145de4.png"
+  },
+  {
+    "revision": "52b16bf8cf82945be4c17c77d6113b71",
+    "url": "/seainfo6150-final-project-webapp/static/media/treefrog.52b16bf8.jpg"
+  }
+]);
