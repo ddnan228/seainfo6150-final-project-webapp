b254035351bf9e53727151403e5e657beb5ec7c7
Doudou Nan <ddnan228@gmail.com>
2020-12-02 10:26:20 -0800

    try to fix github page routing error

--- src/ThankPage/ThankPage.test.js
+++ src/ThankPage/ThankPage.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import ThankPage from "./ThankPage.jsx";
 
-it("renders the AboutUs component correctly", () => {
+it("renders the ThankPage component correctly", () => {
   const { container } = render(<ThankPage />);
   expect(container).toMatchSnapshot();
 });
